e81260db0378bfb093d4d0fff3adc6932d2bf925
Akhmad Faizal P S <akhmadfaizalps@gmail.com>
2019-10-19 22:18:44 +0700

    add navbar

--- gifa/mapservice/admin.py
+++ gifa/mapservice/admin.py
@@ -3,5 +3,48 @@ from django.contrib import admin
 from mapservice.models import MapServices, LayerServices
 
 
-admin.site.register(MapServices)
-admin.site.register(LayerServices)
+class LayerServicesAdmin(admin.ModelAdmin):
+    ordering = ['-layer_name']
+    list_display = [
+        'layer_name',
+        'map_services',
+        'layer_display_name',
+    ]
+    list_display_links = (
+        'layer_name',
+        'map_services',
+        'layer_display_name',
+    )
+    list_filter = [
+        'layer_name',
+    ]
+    search_fields = ['layer_name', 'map_services', 'layer_display_name']
+
+
+class MapServicesAdmin(admin.ModelAdmin):
+    ordering = ['-service_id']
+    list_display = [
+        'service_id',
+        'service_name',
+        'description',
+        'service_attribution',
+        'service_url',
+        'publish'
+    ]
+    list_display_links = (
+        'service_id',
+        'service_name',
+        'description',
+        'service_attribution',
+        'service_url',
+        'publish'
+    )
+    list_filter = [
+        'service_name',
+        'service_id'
+    ]
+    search_fields = ['service_name', 'service_id', 'service_url']
+
+
+admin.site.register(MapServices, MapServicesAdmin)
+admin.site.register(LayerServices, LayerServicesAdmin)
